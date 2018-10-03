25478080a487d31cc7dab0061d84490f5942203f
timothy <timothyawest@gmail.com>
2018-10-03 13:07:49 +0700

    Everything is working... I think. I need to... add numbers change the sound files

--- ShootThemDown/src/main/java/com/example/timothy/shootthemdown/MainActivity.java
+++ ShootThemDown/src/main/java/com/example/timothy/shootthemdown/MainActivity.java
@@ -7,9 +7,17 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
-    private GameSurface gameSurface= null;
-    private long timeOnExit=0;
+    private GameSurface gameSurface = null;
+    private long timeOnExit = 0;
     private int difficulty;
+    private String directory;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -17,48 +25,54 @@ public class MainActivity extends Activity {
         Log.i("Shoebox", "save instance");
 
     }
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         //make the last missile to come update to current time like the gap never happened
 
     }
+
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         gameSurface.pauseGameThread(true);
-        this.timeOnExit =System.nanoTime();
+        this.timeOnExit = System.nanoTime();
 
     }
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-       // if(gameSurface !=null) {
-            int now = (int)(System.nanoTime()/1000000);
-            gameSurface.lastMissile = gameSurface.lastMissile-(int)(timeOnExit/1000000)+now ;
-            gameSurface.pauseGameThread(false);
+        // if(gameSurface !=null) {
+        int now = (int) (System.nanoTime() / 1000000);
+        gameSurface.lastMissileTime = gameSurface.lastMissileTime - (int) (timeOnExit / 1000000) + now;
+        gameSurface.pauseGameThread(false);
 
         //}
 
-        Log.i("Shoebox","start");
+        Log.i("Shoebox", "start");
 
     }
+
     @Override
-    protected void onStop(){
-        Log.i("Shoebox","stop");
+    protected void onStop() {
+        Log.i("Shoebox", "stop");
         super.onStop();
 
     }
+
     @Override
-    protected void onRestart(){
+    protected void onRestart() {
         super.onRestart();
-        Log.i("Shoebox","restart");
+        Log.i("Shoebox", "restart");
 
     }
+
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.i("Shoebox","restored");
+        Log.i("Shoebox", "restored");
 
       /*  if (savedInstanceState != null) {
             Log.i("Shoebox","savedInstanceState");
@@ -78,22 +92,27 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("Shoebox","create");
-        difficulty=getIntent().getIntExtra("DIFFICULTY",0);
-
-            // Set fullscreen
+        Log.i("Shoebox", "create");
+        difficulty = getIntent().getIntExtra("DIFFICULTY", 0);
+        if (getIntent().getStringExtra("DIRECTORY") != null)
+            directory = getIntent().getStringExtra("DIRECTORY");
+        // Set fullscreen
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-            // Set No Title
-       this.gameSurface = new GameSurface(this);
+        // Set No Title
+        this.gameSurface = new GameSurface(this);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(this.gameSurface);
     }
 
-    public int getDifficulty(){
+    public int getDifficulty() {
         return this.difficulty;
     }
 
+    public String getDirectory() {
+        return this.directory;
+    }
+    //public void finish(){finish();}
 }
 
